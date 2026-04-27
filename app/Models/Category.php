5d102b6ed458f11980b8d7d3b8ef91bb4595e4e5
Majid Ali <majid.ali@example.com>
2026-04-27 22:34:01 +0500

    Added factories and expense seeder

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,10 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use App\Models\Expense;
 
 class Category extends Model
 {
+    use HasFactory;
+    
     protected $fillable = ['name'];
 
     // Relationship with Expense
